b0dc8bf106d46722665507ca05917220663282a8
Gian Lazzarini <gianlazzarini@gmail.com>
2018-04-26 22:28:51 -0700

    Added a Mentor Poco to the domain layer & now the CLI makes a new mentor, adds them to the db, then attempts to message them through sms

--- HackathonManager.Cli/Program.cs
+++ HackathonManager.Cli/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HackathonManager.Sms;
+using HackathonManager.DIContext;
 
 namespace HackathonManager.Cli
 {
@@ -11,7 +11,27 @@ namespace HackathonManager.Cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(TwilioSmsService.SendSms(4255899296, Console.ReadLine()));
+            Console.WriteLine("Add new mentors to the system:");
+            var mentor = new DTO.Mentor();
+            Console.WriteLine("Name:");
+            mentor.Name = Console.ReadLine();
+            Console.WriteLine("Age(accepts integer):");
+            mentor.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Description:");
+            mentor.Description = Console.ReadLine();
+            Console.WriteLine("PhoneNumber:");
+            mentor.PhoneNumber = Console.ReadLine();
+            mentor.IsAvailable = true;
+            mentor.IsPresent = true;
+
+            var db = Context.GetMLabsMongoDbRepo();
+            db.Add(mentor);
+
+            var mentorFromDb = db.Single<DTO.Mentor>(x => x.PhoneNumber == mentor.PhoneNumber);
+
+            var SmsService = Context.GetTwilioSmsService();
+            Console.WriteLine(SmsService.SendSms(int.Parse(mentorFromDb.PhoneNumber), Console.ReadLine()));
+
             Console.ReadKey();
         }
     }
---
+++ HackathonManager/DTO/Mentor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonManager.DTO
+{
+    public class Mentor
+    {
+        public System.Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsPresent { get; set; }
+    }
+}
