516d7bc6188408180383495472a2c6a784c7a464
Gian Lazzarini <gianlazzarini@gmail.com>
2018-04-29 13:08:44 -0700

    Changes to check if the mentor is registered in the db already by their name or phonenumber

--- HackathonManager.Cli/Program.cs
+++ HackathonManager.Cli/Program.cs
@@ -26,23 +26,42 @@ namespace HackathonManager.Cli
 
             //var db = Context.GetMLabsMongoDbRepo();
             var db = Context.GetLocalMongoDbRepo();
-            db.Add(mentor);
 
-            var mentorFromDb = db.Single<DTO.Mentor>(x => x.PhoneNumber == mentor.PhoneNumber);
+            //Check to see if this person is already in the db
+            if (db.Single<DTO.Mentor>(x => x.Name == mentor.Name) != null)
+            {
+                Console.WriteLine($"{mentor.Name} has already been registered.");
+                Console.ReadKey();
+                return;
+            }
+            if (db.Single<DTO.Mentor>(x => x.PhoneNumber == mentor.PhoneNumber) != null)
+            {
+                Console.WriteLine($"{mentor.PhoneNumber} has already been registered.");
+                Console.ReadKey();
+                return;
+            }
+            try
+            {
+                db.Add(mentor);
+                var mentorFromDb = db.Single<DTO.Mentor>(x => x.Name == mentor.Name);
 
-            var smsService = Context.GetTwilioSmsService();
-            Console.WriteLine(smsService.SendSms(uint.Parse(mentorFromDb.PhoneNumber),
-$@"{mentorFromDb.Name}, you've been added in and registered as a mentor
-for this event. 
-You'll recieve prompts via sms from here out for your instructions. 
-After finishing a mentoring task it will be your responsability to
-message this number again saying FINISHED. That way you'll be able 
-to signify availability again.
-If you don't set yourself as FINISHED within the first 20 min you will
-be prompted to see if you're done every 15 minutes there out until you are."));
+                var smsService = Context.GetTwilioSmsService();
+                Console.WriteLine(smsService.SendSms(uint.Parse(mentorFromDb.PhoneNumber),
+                $"{mentorFromDb.Name}, you've been added in and registered as a mentor for this event. " +
+                $"You'll recieve prompts via sms from here out for your instructions. " +
+                $"After finishing a mentoring task it will be your responsability to " +
+                $"message this number again saying FINISHED. That way you'll be able " +
+                $"to signify availability again. " +
+                $"If you don't set yourself as FINISHED within the first 20 min you will " +
+                $"be prompted to see if you're done every 15 minutes there out until you are."));
 
-            Console.WriteLine($"Introductory sms sent to {mentor.Name} at {mentor.PhoneNumber}.");
-            Console.ReadKey();
+                Console.WriteLine($"Introductory sms sent to {mentor.Name} at {mentor.PhoneNumber}.");
+                Console.ReadKey();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
         }
     }
 }
