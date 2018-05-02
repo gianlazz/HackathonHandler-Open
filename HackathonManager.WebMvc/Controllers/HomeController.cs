48343f2bc1c32001264114cd5e5f714211f8e1c4
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-01 18:46:10 -0700

    Added in new basic Bootstrap 3 mvc project with MongoDb

--- HackathonManager.WebMvc/Controllers/HomeController.cs
+++ HackathonManager.WebMvc/Controllers/HomeController.cs
@@ -3,27 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HackathonManager.Persistance;
-using HackathonManager.Mvc.Models;
-//using HackathonManager.DIContext;
+using HackathonManager.DIContext;
+using HackathonManager.DTO;
 
-namespace HackathonManager.Mvc.Controllers
+namespace HackathonManager.WebMvc.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            //HackathonManagerEntities db = new HackathonManagerEntities();
-            //MentorViewModel mentorViewModel = new MentorViewModel();
 
-            //mentorViewModel.Mentors =  db.Mentors.ToList();
-            //mentorViewModel.PresentMentors = db.Mentors.Where(o => o.IsPresent == true).ToList();
-            //mentorViewModel.AvailableMentors = db.Mentors.Where(o => o.IsAvailable == true).ToList();
-
-            ////var repo = Context.GetMLabsMongoDbRepo();
-            ////mentorViewModel.Mentors = repo.All<Mentor>() as List<Mentor>;
-            ////mentorViewModel.PresentMentors = repo.All<Mentor>().Where(x => x.IsPresent == true).ToList();
-            ////mentorViewModel.AvailableMentors = repo.All<Mentor>().Where(x => x.IsAvailable == true).ToList();
             var Db = MvcApplication.DbRepo;
 
             //var db = Context.GetMLabsMongoDbRepo();
---
+++ HackathonManager.WebMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackathonManager.WebMvc.MvcApplication" Language="C#" %>
