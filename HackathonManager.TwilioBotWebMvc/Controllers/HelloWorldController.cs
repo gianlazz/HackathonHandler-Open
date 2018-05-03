be2513e74be193780b4287bda57109fcc13074c3
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-02 20:27:17 -0700

    Added further model binding adjustments to the TwilioBotwebMvc project HelloWorldController

--- HackathonManager.TwilioBotWebMvc/Controllers/HelloWorldController.cs
+++ HackathonManager.TwilioBotWebMvc/Controllers/HelloWorldController.cs
@@ -61,14 +61,20 @@ namespace HackathonManager.TwilioBotWebMvc.Controllers
          */
 
         [HttpPost]
-        public void Index(string From, string Body)
+        public void Index(SmsRequest request, string To, string From, string Body)
         {
             var smsDto = new SmsDto();
+            smsDto.Sid = request.SmsSid;
             smsDto.DateCreated = DateTime.Now;
-            smsDto.MessageBody = Body;
-            //smsDto.Sid = request.SmsSid;
-            smsDto.ToPhoneNumber = "+12065086735";
+            smsDto.ToPhoneNumber = To;
             smsDto.FromPhoneNumber = From;
+            smsDto.MessageBody = Body;
+
+            //smsDto.DateCreated = DateTime.Now;
+            //smsDto.MessageBody = Body;
+            ////smsDto.Sid = request.SmsSid;
+            //smsDto.ToPhoneNumber = "+12065086735";
+            //smsDto.FromPhoneNumber = From;
 
             var Db = MvcApplication.DbRepo;
 
