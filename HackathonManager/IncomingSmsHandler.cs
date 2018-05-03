b5653ca7dc0ebb22a730ec0f4da3e90e991e2cd2
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-02 20:47:37 -0700

    Stubbed out an IncomingSmsHandler class with a Process(SmsDTO) method

---
+++ HackathonManager/IncomingSmsHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackathonManager.DTO;
+
+namespace HackathonManager
+{
+    public class IncomingSmsHandler
+    {
+        public IncomingSmsHandler()
+        {
+
+        }
+
+        public void Process(SmsDto incomingSmsDto)
+        {
+            
+        }
+    }
+}
