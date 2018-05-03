be3b93605ed46aaa6d7f81b72d3a187d2822bd60
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-02 19:51:03 -0700

    Added new Controller method on Twilio Mvc bot to bind right to the TwilioResponse SMSMessage properties along with a comment of what that looks like

--- HackathonManager.TwilioBotWebMvc/Controllers/HelloWorldController.cs
+++ HackathonManager.TwilioBotWebMvc/Controllers/HelloWorldController.cs
@@ -13,6 +13,30 @@ namespace HackathonManager.TwilioBotWebMvc.Controllers
 {
     public class HelloWorldController : TwilioController
     {
+        /* 
+            <TwilioResponse>
+                <SMSMessage>
+                    <Sid>SM1f0e8ae6ade43cb3c0ce4525424e404f</Sid>
+                    <DateCreated>Fri, 13 Aug 2010 01:16:24 +0000</DateCreated>
+                    <DateUpdated>Fri, 13 Aug 2010 01:16:24 +0000</DateUpdated>
+                    <DateSent/>
+                    <AccountSid>AC228b97a5fe4138be081eaff3c44180f3</AccountSid>
+                    <To>+13455431221</To>
+                    <From>+15104564545</From>
+                    <Body>A Test Message</Body>
+                    <Status>queued</Status>
+                    <Flags>
+                        <Flag>outbound</Flag>
+                    </Flags>
+                    <ApiVersion>2010-04-01</ApiVersion>
+                    <Price/>
+                    <Uri>
+                        /2010-04-01/Accounts/AC228b97a5fe4138be081eaff3c44180f3/SMS/Messages/SM1f0e8ae6ade43cb3c0ce4525424e404f
+                    </Uri>
+                </SMSMessage>
+            </TwilioResponse>
+         */
+
         // GET: HelloWorld
         //[HttpPost]
         //public TwiMLResult Index(SmsRequest request)
@@ -22,14 +46,28 @@ namespace HackathonManager.TwilioBotWebMvc.Controllers
         //    return TwiML(response);
         //}
 
-        public void Index(SmsRequest request)
+        //public void Index(SmsRequest request)
+        //{
+        //    var smsDto = new SmsDto();
+        //    smsDto.DateCreated = DateTime.Now;
+        //    smsDto.MessageBody = request.Body;
+        //    smsDto.Sid = request.SmsSid;
+        //    smsDto.ToPhoneNumber = "+12065086735";
+        //    smsDto.FromPhoneNumber = String.Empty;
+
+        //    var Db = MvcApplication.DbRepo;
+
+        //    Db.Add<SmsDto>(smsDto);
+        //}
+
+        public void Index(string From, string Body)
         {
             var smsDto = new SmsDto();
             smsDto.DateCreated = DateTime.Now;
-            smsDto.MessageBody = request.Body;
-            smsDto.Sid = request.SmsSid;
+            smsDto.MessageBody = Body;
+            //smsDto.Sid = request.SmsSid;
             smsDto.ToPhoneNumber = "+12065086735";
-            smsDto.FromPhoneNumber = String.Empty;
+            smsDto.FromPhoneNumber = From;
 
             var Db = MvcApplication.DbRepo;
 
