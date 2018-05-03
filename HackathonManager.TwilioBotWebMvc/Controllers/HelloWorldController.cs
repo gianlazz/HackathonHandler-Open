4ab77ac139018f033244d73ad6dc6312a8c5250b
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-02 19:26:55 -0700

    Building out TwilioBotWebMvc project from twilio example for recieving sms with webhooks

---
+++ HackathonManager.TwilioBotWebMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+using HackathonManager.DTO;
+using HackathonManager.RepositoryPattern;
+
+namespace HackathonManager.TwilioBotWebMvc.Controllers
+{
+    public class HelloWorldController : TwilioController
+    {
+        // GET: HelloWorld
+        //[HttpPost]
+        //public TwiMLResult Index(SmsRequest request)
+        //{
+        //    var response = new MessagingResponse();
+        //    response.Message("Hello World");
+        //    return TwiML(response);
+        //}
+
+        public void Index(SmsRequest request)
+        {
+            var smsDto = new SmsDto();
+            smsDto.DateCreated = DateTime.Now;
+            smsDto.MessageBody = request.Body;
+            smsDto.Sid = request.SmsSid;
+        }
+    }
+}
---
+++ HackathonManager.TwilioBotWebMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackathonManager.TwilioBotWebMvc.MvcApplication" Language="C#" %>
