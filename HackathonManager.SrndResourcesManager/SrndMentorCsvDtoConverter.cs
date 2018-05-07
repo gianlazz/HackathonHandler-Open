6d9183613f22306138baafa22c8ba4d33d884890
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-06 22:49:52 -0700

    Build out more test of the MentorCsvConverter

--- HackathonManager.SrndResourcesManager/SrndMentorCsvDtoConverter.cs
+++ HackathonManager.SrndResourcesManager/SrndMentorCsvDtoConverter.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HackathonManager.DTO;
 
 namespace HackathonManager
 {
-    class SrndMentorCsvDtoConverter
+    public class SrndMentorCsvDtoConverter
     {
+        /* [0] = event
+        * [1] = type
+        * [2] = lastname
+        * [3] = firstname
+        * [4] = email
+        * [5] = age
+        * [6] = promocode
+        * [7] = paid
+        * [8] = parentname
+        * [9] = parentemail
+        * [10] = parentphone
+        * [11] = parentphonealt
+        * [12] = checkedin
+        * [13] = created
+        */
+
+        public List<Mentor> Parse(string csv)
+        {
+            var mentors = new List<Mentor>();
+            var mentorCsvLines = new List<string>();
+
+            using (var reader = new StringReader(csv))
+            {
+                for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                {
+                    //Skip the first header line
+                    if (line.Contains("event"))
+                        continue;
+                    mentorCsvLines.Add(line);
+                }
+            }
+
+            foreach (var mentor in mentorCsvLines)
+            {
+                mentors.Add(new Mentor
+                {
+                    GuidId = new Guid,
+
+                });
+            }
+
+            return null;
+        }
     }
 }
--- HackathonManager/PocoModels/Mentor.cs
+++ HackathonManager/PocoModels/Mentor.cs
@@ -9,7 +9,8 @@ namespace HackathonManager.DTO
     public class Mentor
     {
         public System.Guid GuidId { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Image { get; set; }
         public int Age { get; set; }
         public string Description { get; set; }
