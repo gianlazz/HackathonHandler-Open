821f32932afc5bb399d403c455af8796374d6fbb
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-07 21:18:08 -0700

    Got the basics of the admin pages stubbed out and a simple home overview page with mongodb details for the objects

---
+++ HackathonManager.AdminWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HackathonManager.DTO;
+using HackathonManager.PocoModels;
+using HackathonManager.AdminWebMvc.Models;
+
+namespace HackathonManager.AdminWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            OverViewModel overView = new OverViewModel();
+            overView.Mentors = new List<Mentor>();
+            overView.Judges = new List<Judge>();
+            overView.Teams = new List<Team>();
+            var repo = MvcApplication.DbRepo;
+
+            //overView.Mentors.AddRange(repo.All<Mentor>().Where(x => x.Event == "seattle-eastside"));
+            try
+            {
+                overView.Mentors.AddRange(repo.All<Mentor>().ToList());
+                overView.Judges.AddRange(repo.All<Judge>().ToList());
+                overView.Teams.AddRange(repo.All<Team>().ToList());
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            return View(overView);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Sms()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Teams()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Judges()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ HackathonManager.AdminWebMvc/Models/OverViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HackathonManager.DTO;
+using HackathonManager.PocoModels;
+
+namespace HackathonManager.AdminWebMvc.Models
+{
+    public class OverViewModel
+    {
+        public List<Mentor> Mentors { get; set; }
+        public List<Judge> Judges { get; set; }
+        public List<Team> Teams { get; set; }
+    }
+}
