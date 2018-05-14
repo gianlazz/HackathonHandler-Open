9765163f80735ec9ad71d238831d835e3481f0cf
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-13 18:09:29 -0700

    Setup the Sms page index table view of messages

---
+++ HackathonManager.AdminWebMvc/Controllers/SmsController.cs
@@ -0,0 +1,22 @@
+﻿using HackathonManager.DTO;
+using HackathonManager.RepositoryPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackathonManager.AdminWebMvc.Controllers
+{
+    public class SmsController : Controller
+    {
+        private IRepository _repo = MvcApplication.DbRepo;
+        // GET: Mentors
+        public ActionResult Index()
+        {
+            var messages = _repo.All<SmsDto>().ToList();
+
+            return View(messages);
+        }
+    }
+}
