71950388f81b93351ee5fcf67cfe9d537acbfa77
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-20 15:18:14 -0700

    Now landing page orderes by IsAvailable

--- HackathonManager.WebMvc/Controllers/HomeController.cs
+++ HackathonManager.WebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace HackathonManager.WebMvc.Controllers
 
             //var db = Context.GetMLabsMongoDbRepo();
             var model = new List<Mentor>();
-            model = Db.All<Mentor>().ToList();
+            model = Db.All<Mentor>().Where(x => x.IsPresent == true).ToList().OrderBy(e => e.IsAvailable ? 0 : 1).ToList();
+            //model = Db.All<Mentor>().Where(x => x.IsPresent == true).ToList();
+
             //mentorViewModel.PresentMentors = repo.All<Mentor>().Where(x => x.IsPresent == true).ToList();
             //mentorViewModel.AvailableMentors = repo.All<Mentor>().Where(x => x.IsAvailable == true).ToList();
 
