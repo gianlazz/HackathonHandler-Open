b35e6d101b6f2d5b17fb60eeac54c96b158f6d67
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-20 18:28:31 -0700

    Hid properties from the tables for Judges and Teams on the user frontend

--- HackathonManager.WebMvc/Controllers/JudgesController.cs
+++ HackathonManager.WebMvc/Controllers/JudgesController.cs
@@ -18,76 +18,76 @@ namespace HackathonManager.WebMvc.Controllers
             return View(judges);
         }
 
-        // GET: Judges/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+        //// GET: Judges/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
 
-        // GET: Judges/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Judges/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: Judges/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+        //// POST: Judges/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: Judges/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //// GET: Judges/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: Judges/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+        //// POST: Judges/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: Judges/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //// GET: Judges/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: Judges/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+        //// POST: Judges/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
--- HackathonManager.WebMvc/Controllers/TeamsController.cs
+++ HackathonManager.WebMvc/Controllers/TeamsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HackathonManager.PocoModels;
+using HackathonManager.RepositoryPattern;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,13 @@ namespace HackathonManager.WebMvc.Controllers
 {
     public class TeamsController : Controller
     {
+        private IRepository _Db = MvcApplication.DbRepo;
+
         // GET: Teams
         public ActionResult Index()
         {
-            return View();
+            var teams = _Db.All<Team>();
+            return View(teams);
         }
     }
 }
