0d9c3e27a8cfe32030fc9906153d9d593a135564
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-27 18:07:21 -0700

    Created UniqueTeamFactory with public InstantiateUniquely() method

---
+++ HackathonManager/UniqueTeamFactory.cs
@@ -0,0 +1,24 @@
+﻿using HackathonManager.PocoModels;
+using HackathonManager.RepositoryPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonManager
+{
+    public class UniqueTeamFactory
+    {
+        private int _pinNumber;
+        public UniqueTeamFactory(IRepository repository)
+        {
+            _pinNumber = new PinGenerator(repository).GenerateNewPin(new Team());
+        }
+
+        public Team InstantiateUniquely()
+        {
+            return new Team() { PinNumber = _pinNumber };
+        }
+    }
+}
