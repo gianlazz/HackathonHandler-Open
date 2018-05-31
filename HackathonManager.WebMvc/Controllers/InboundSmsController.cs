09b48f1d5e7d4bc2000c7dc71b8f9f170f8138eb
Gian Lazzarini <gianlazzarini@gmail.com>
2018-05-30 18:37:15 -0700

    Added Twilio InboundSmsController to the primary WebMvc front end project for easier managment of inbound posted messages

---
+++ HackathonManager.WebMvc/Controllers/InboundSmsController.cs
@@ -0,0 +1,45 @@
+﻿using HackathonManager.DTO;
+using HackathonManager.Sms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace HackathonManager.WebMvc.Controllers
+{
+    public class InboundSmsController : Controller
+    {
+        // GET: InboundSms
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Index(SmsRequest request, string To, string From, string Body)
+        {
+            var smsDto = new SmsDto();
+            smsDto.Sid = request.SmsSid;
+            smsDto.DateCreated = DateTime.Now;
+            smsDto.ToPhoneNumber = To;
+            smsDto.FromPhoneNumber = From;
+            smsDto.MessageBody = Body;
+
+            //smsDto.DateCreated = DateTime.Now;
+            //smsDto.MessageBody = Body;
+            ////smsDto.Sid = request.SmsSid;
+            //smsDto.ToPhoneNumber = "+12065086735";
+            //smsDto.FromPhoneNumber = From;
+
+            var Db = MvcApplication._dbRepo;
+
+            Db.Add<SmsDto>(smsDto);
+            SmsRoutingConductor.InboundMessages.Enqueue(smsDto);
+        }
+    }
+}
